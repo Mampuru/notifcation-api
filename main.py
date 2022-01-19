8e9478200cd46724d9b5863470e7b2f120fca20c
Mampuru <mampuruntwampe@gmail.com>
2022-01-19 22:32:01 +0200

    Added the email model and post method

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
+
+class EmailModel(BaseModel):
+    name: str
+    contact: str
+    message: str
+
 
 @app.get("/")
 def hello():
-    return {"message": "FastAPI 101"}
+    return {"message": "FastAPI 101 START"}
+
+
+@app.post("/email-server")
+def send_email(emailModel: EmailModel):
+    return {"code":"200","name": emailModel.name,"contact": emailModel.contact,"message": emailModel.message}
