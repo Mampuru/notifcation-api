92bdab6ce60108d8bbaf3b898288d9a577173527
Mampuru <mampuruntwampe@gmail.com>
2022-01-19 22:51:30 +0200

    Added the mailjet email endpoint

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from mailjet_rest import Client
+import os
 app = FastAPI()
+
+api_key ='<PLACE KEY HERE>'
+api_secret= '<PLACE SECRET HERE>'
 
 class EmailModel(BaseModel):
     name: str
     contact: str
     message: str
 
+mailjet = Client(auth=(api_key, api_secret), version='v3.1')
 
 @app.get("/")
 def hello():
@@ -15,4 +21,25 @@ def hello():
 
 @app.post("/email-server")
 def send_email(emailModel: EmailModel):
-    return {"code":"200","name": emailModel.name,"contact": emailModel.contact,"message": emailModel.message}
+    data = {
+        'Messages': [
+            {
+            "From": {
+                "Email": "<PLACE EMAIL HERE>",
+                "Name": "<PLACE NAME HERE>"
+            },
+            "To": [
+                {
+                "Email": "<PLACE RECEIVE EMAIL HERE>",
+                "Name": "<PLACE NAME HERE>"
+                }
+            ],
+            "Subject": "<PLACE SUBJECT HERE>",
+            "TextPart": "<PLACE MESSAGE BODY HERE>",
+            "HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+            "CustomID": "AppGettingStartedTest"
+            }
+        ]
+        }
+    result = mailjet.send.create(data=data)
+    return {"response": result.status_code}
